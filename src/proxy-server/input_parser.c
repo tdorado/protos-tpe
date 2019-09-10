fddede9e0233cdf5112e75be3c9325830f424126
Tomas Dorado <tdorado@itba.edu.ar>
2019-09-10 16:47:20 -0300

    Minor fix input parser

--- src/proxy-server/input_parser.c
+++ src/proxy-server/input_parser.c
@@ -1,11 +1,9 @@
 #include <ctype.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <unistd.h>
-#include <netdb.h>
+#include <getopt.h>
 
 #include "include/input_parser.h"
 #include "include/constants.h"
