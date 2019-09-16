0d7de7ef2c670aa08eaab86ad1b5e2991dba9225
JoaquinBattilana <jbattilana@itba.edu.ar>
2019-09-16 03:59:50 -0300

    created extract body function

--- src/proxy-server/external_transformation.c
+++ src/proxy-server/external_transformation.c
@@ -7,6 +7,26 @@
 // A EL STDIN DE UN PROCESO QUE EJECUTE EL CMD QUE ESTA EN PROXY_PARAMS->CMD, Y SE VA A TENER OTRO
 // PIPE QUE SE CONECTE CON EL STDOUT DEL PROCESO QUE RECIBE EL TEXTO Y LO VUELVE A PARSEAR CON LOS . EN MODO POP3
 
-char * transform(char * transform_command , char * email) {
+char * transform(char * transform_command , char * buffer, int buffer_size) {
+    char * message_body = extract_body(email, buffer_size);
+    int fd[2];
+    pipe(fd);
+    if (fork()) {
+        close(fd[])
+    }
     return transform_command;
+}
+
+char * extract_body(char * buffer, int buffer_size) {
+    int i=0;
+    char * body = malloc(buffer_size * sizeof(char));
+    while(email[i] != '\n' && i < buffer_size) {
+        body[i] = buffer[i];
+        i++;
+    }
+    if(i == buffer_size) {
+        free(body);
+        return NULL;
+    }
+    return body;
 }
--- src/proxy-server/include/external_transformation.h
+++ src/proxy-server/include/external_transformation.h
@@ -1,6 +1,8 @@
 #ifndef EXTERNAL_TRANSFORMATIONS_H
 #define EXTERNAL_TRANSFORMATIONS_H
 
+// function that extract body from pop3 message recived by server ( extract first line )
+char * extract_body(char * buffer, int buffer_size);
 char * transform(char * transform_command , char * email);
 
 #endif
