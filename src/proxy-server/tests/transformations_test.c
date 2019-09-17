248f4e8c3b25e6ca8a6694f92e7d602459242ca0
JoaquinBattilana <jbattilana@itba.edu.ar>
2019-09-17 04:26:28 -0300

    extract pop3 info working

---
+++ src/proxy-server/tests/transformations_test.c
@@ -0,0 +1,10 @@
+#include "../external_transformation.c"
+
+int main(void) {
+    char * testPop3 = "+OK 123\r\nhola como estas\r\n.\r\n";
+    char * head = malloc(100);
+    char * body = malloc(100);
+    extract_pop3_info(testPop3, 100, head, body);
+    printf("head: %s", head);
+    printf("body: %s", body);
+}
