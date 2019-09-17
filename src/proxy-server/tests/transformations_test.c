ac1b8b770ae572e55165c9827ea9ee2a70a6727b
JoaquinBattilana <jbattilana@itba.edu.ar>
2019-09-17 04:39:32 -0300

    fixed pop3_to_text

--- src/proxy-server/tests/transformations_test.c
+++ src/proxy-server/tests/transformations_test.c
@@ -1,10 +1,14 @@
 #include "../external_transformation.c"
 
 int main(void) {
-    char * testPop3 = "+OK 123\r\nhola como estas\r\n.\r\n";
+    char * testPop3 = "+OK 123\r\nhola como ..... estas\r\n.\r\n";
     char * head = malloc(100);
     char * body = malloc(100);
     extract_pop3_info(testPop3, 100, head, body);
     printf("head: %s", head);
     printf("body: %s", body);
+    printf("\n SEPARADOR LINEA \n");
+    char * normal_text = malloc(100);
+    pop3_to_text(body, 100, normal_text);
+    printf("parsed text: %s", normal_text);
 }
