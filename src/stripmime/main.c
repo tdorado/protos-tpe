4a59421b72fc4a5b7b53c298e560e88f1b37189f
Joaquin Battilana <battilanajn@gmail.com>
2019-11-10 04:24:47 -0300

    checking variables working

--- src/stripmime/main.c
+++ src/stripmime/main.c
@@ -2,12 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ENV_VARIABLES_QUANTITY 2
+#define FAIL -1
+#define SUCCESS 1
+
+const char * ENV_VARIABLES[] = {"FILTER_MEDIAS", "FILTER_MSG"};
+
+int check_variables();
+
 int main(void) {
-    const char * ENV_VARIABLES[] = {"FILTER_MEDIAS", "FILTER_MSG"};
-    printf("FILTER_MEDIAS: %s \n", getenv(ENV_VARIABLES[0]));
-    printf("FILTER_MSG: %s \n", getenv(ENV_VARIABLES[1]));
-    char * filter_medias = getenv(ENV_VARIABLES[0]);
-    if(filter_medias == NULL) {
-        printf("Hola");
+    if(check_variables() == FAIL) {
+        return FAIL;
     }
+}
+
+int check_variables() {
+    for(int i = 0; i < ENV_VARIABLES_QUANTITY; i++) {
+        if(getenv(ENV_VARIABLES[0]) == NULL) {
+            fprintf(stderr, "La variable %s no está definida \n", ENV_VARIABLES[i]);
+            return FAIL;
+        }
+    }
+    return SUCCESS;
 }
