df8824af9223ba0c9b560a303fa5124aba55a175
Joaquin Battilana <battilanajn@gmail.com>
2019-11-10 04:20:04 -0300

    created null for variables

--- src/stripmime/main.c
+++ src/stripmime/main.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main(void) {
-    char * ENV_VARIABLES[] = ["FILTER_MEDIAS", "FILTER_MSG"];
+    const char * ENV_VARIABLES[] = {"FILTER_MEDIAS", "FILTER_MSG"};
+    printf("FILTER_MEDIAS: %s \n", getenv(ENV_VARIABLES[0]));
+    printf("FILTER_MSG: %s \n", getenv(ENV_VARIABLES[1]));
+    char * filter_medias = getenv(ENV_VARIABLES[0]);
+    if(filter_medias == NULL) {
+        printf("Hola");
+    }
 }
