a349cfd7e548ee0b4792393bb3cff39031ecea26
Joaquin Battilana <battilanajn@gmail.com>
2019-11-10 05:31:41 -0300

    added printing headers

--- src/stripmime/main.c
+++ src/stripmime/main.c
@@ -7,6 +7,14 @@
 #define SUCCESS 1
 #define TRUE 1
 #define FALSE 0
+#define BLOCK 10
+#define MULTIPART "multipart"
+#define DEFAULT_MIME_VERSION "1.0"
+#define DEFAULT_MIME "text/plain"
+#define HEADER_TO_BODY "\n\n"
+#define NEW_LINE '\n'
+#define CONTENT_TYPE "Content-Type: "
+#define CONTENT_TYPE_LENGTH 14
 
 const char * ENV_VARIABLES[] = {"FILTER_MEDIAS", "FILTER_MSG"};
 
@@ -18,8 +26,11 @@ int main(void) {
         return FAIL;
     }
     char * filter_mime = getenv(ENV_VARIABLES[0]);
-    if(check_mime(filter_mime) == FAIL)
+    int slash_position = -1;
+    if(check_mime(filter_mime, &slash_position) == FAIL)
         fprintf(stderr, "El mime para filtrar está mal definido");
+    char * mime_to_filter = NULL;
+    
 }
 
 int check_variables() {
@@ -32,15 +43,43 @@ int check_variables() {
     return SUCCESS;
 }
 
-int check_mime(char * mime) {
+int check_mime(char * mime, int * slash_position) {
     int slash_flag = FALSE;
     for(int i = 0; mime[i] != '\0'; i++) {
-        if(mime[i] = '/')
+        if(mime[i] == '/')
             if(slash_flag == TRUE)
                 return FAIL;
+            *slash_position = i;
             slash_flag = TRUE;
     }
     if(slash_flag = 1)
         return SUCCESS;
     return FAIL;
+}
+
+int check_pop3_headers() {
+    int finished_head = FALSE;
+    int head_position = 0;
+    while( (c = getchar()) != EOF && !finished_head) {
+        putchar(c);
+        if( c == CONTENT_TYPE[head_position])
+            head_position++;
+        else {
+            skip_to_new_line();
+            head_position = 0;
+        }
+        if(head_position == CONTENT_TYPE_LENGTH) {
+
+        }
+}
+
+int skip_to_new_line() {
+    int c;
+    int got_new_line = FALSE;
+    while( (c = getchar()) != EOF && !got_new_line ) {
+        putchar(c);
+        if( c == '\n') 
+            got_new_line == TRUE;
+    }
+    return got_new_line;
 }
