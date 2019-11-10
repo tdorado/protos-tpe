e7704dd67b29c5898c2906c2ddee8eebc862e160
Joaquin Battilana <battilanajn@gmail.com>
2019-11-10 04:37:54 -0300

    created check mime

--- src/stripmime/main.c
+++ src/stripmime/main.c
@@ -9,19 +9,36 @@
 const char * ENV_VARIABLES[] = {"FILTER_MEDIAS", "FILTER_MSG"};
 
 int check_variables();
+int check_mime(char *);
 
 int main(void) {
     if(check_variables() == FAIL) {
         return FAIL;
     }
+    char * filter_mime = getenv(ENV_VARIABLES[0]);
+    if(check_mime(filter_mime) == FAIL)
+        fprintf(stderr, "El mime para filtrar está mal definido");
 }
 
 int check_variables() {
     for(int i = 0; i < ENV_VARIABLES_QUANTITY; i++) {
-        if(getenv(ENV_VARIABLES[0]) == NULL) {
+        if(getenv(ENV_VARIABLES[i]) == NULL) {
             fprintf(stderr, "La variable %s no está definida \n", ENV_VARIABLES[i]);
             return FAIL;
         }
     }
     return SUCCESS;
+}
+
+int check_mime(char * mime) {
+    int slash_flag = 0;
+    for(int i = 0; mime[i] != '\0'; i++) {
+        if(mime[i] = '/')
+            if(slash_flag == 1)
+                return FAIL;
+            slash_flag = 1;
+    }
+    if(slash_flag = 1)
+        return SUCCESS;
+    return FAIL;
 }
