0b970070018871b3259a41c4a356498407777d8c
Joaquin Battilana <battilanajn@gmail.com>
2019-11-11 22:55:23 -0300

    created basic queue

--- src/stripmime/headers.c
+++ src/stripmime/headers.c
@@ -19,7 +19,6 @@
 typedef struct content_type_header {
     char content_type[MAX_CONTENT_TYPE];
     char boundary[MAX_BOUNDARY_TYPE];
-    int boundary_length;
 } content_type_header;
 
 typedef struct content_type_header * content_type_header_t;
@@ -30,6 +29,43 @@ int skip_to_body();
 int handle_attributes(content_type_header_t);
 int manage_multipart(content_type_header_t, char *, char *);
 int search_boundary(char *, int );
+
+typedef struct node * node_t;
+typedef struct stack * stack_t;
+
+typedef struct node {
+    content_type_header_t elem;
+    node_t next;
+} node ;
+
+typedef struct stack {
+    node_t head;
+} stack;
+
+stack_t create_stack() {
+    stack_t stack = malloc(sizeof(stack));
+    stack->head = NULL;
+    return stack;
+}
+
+void stack_push(stack_t stack, content_type_header_t elem) {
+    node_t node = malloc(sizeof(node));
+    node->elem = elem;
+    node->next = stack->head;
+    stack->head = node;
+}
+
+content_type_header_t stack_pop(stack_t stack) {
+    if(stack->head != NULL) {
+        node_t aux = stack->head;
+        content_type_header_t elem = aux->elem;
+        stack->head = stack->head->next;
+        free(aux);
+        return elem;
+    }
+    return NULL;
+}
+
 
 int main(void) {
     content_type_header_t content_type = malloc(sizeof(content_type_header));
@@ -110,7 +146,6 @@ int handle_attributes(content_type_header_t content_type) {
     int c;
     c = getchar();
     int boundary_length = scanf(" boundary=%s", content_type->boundary);
-    content_type->boundary_length = boundary_length;
     printf(" boundary=%s\r\n", content_type->boundary);
     getchar();
     getchar();
@@ -159,7 +194,7 @@ int manage_multipart(content_type_header_t content_type, char * replace_mime, ch
     content_type_header_t new_content_type = malloc(sizeof(content_type_header));
     int response;
     int print = TRUE;
-    while((response = search_boundary(content_type->boundary, print)) != EOF) {
+    while((response = search_boundary(content_type->boundary, print)) != EOF && response != FINAL_BOUNDARY) {
         if(response == START_BOUNDARY) {
             headers(new_content_type);
             if(strncmp(new_content_type->content_type, "multipart/", 10) == 0) {
@@ -172,9 +207,6 @@ int manage_multipart(content_type_header_t content_type, char * replace_mime, ch
             else {
                 print = TRUE;
             }
-        }
-        else if(response == FINAL_BOUNDARY) {
-            break;
         }
     }
     free(new_content_type);
