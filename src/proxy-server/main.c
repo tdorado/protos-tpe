4bfddfb2f7f533785e1556fd507a349b66075c48
Tomas Dorado <tdorado@itba.edu.ar>
2019-11-13 20:00:12 -0300

    Limpieza

--- src/proxy-server/main.c
+++ src/proxy-server/main.c
@@ -1,25 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-#include <errno.h>
-#include <netdb.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <netdb.h>
 #include <signal.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/select.h>
-#include <sys/socket.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/ioctl.h>
-#include <net/if.h>
-#include <netinet/sctp.h>
-#include <netinet/in.h>
+#include <errno.h>
 
 #include "include/proxy_socket.h"
 #include "include/settings.h"
